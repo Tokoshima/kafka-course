8b33af33ea60b21960463aeac728d2d1e7db1220
LouwPretorius <louw.pretorius@monocle.co.za>
2025-03-03 12:10:28 +0200

    latest working with eventsource 4+

--- kafka-beginners-course/kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-beginners-course/kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,15 +1,18 @@
 package io.conduktor.demos.kafka.wikimedia;
 
 import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class WikimediaChangesProducer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String boostrapServers = "127.0.0.1:9092";
 
@@ -22,10 +25,21 @@ public class WikimediaChangesProducer {
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
-        EventSource eventHandler = TODO;
+        String topic = "wikimedia.recentchange";
+
+        BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
         String url = "https://stream.wikimedia.org/v2/stream/recentchange";
-        EventSource.Builder builder = new EventSource.Builder(eventHandler, url);
-        EventSource eventSource = builder.build();
+
+        EventSource.Builder eventSourceBuilder = new EventSource.Builder(URI.create(url));
+
+        BackgroundEventSource.Builder builder = new BackgroundEventSource.Builder(eventHandler, eventSourceBuilder);
+
+        BackgroundEventSource eventSource = builder.build();
+
+        //Starts in another thread
         eventSource.start();
+
+        // r
+        TimeUnit.MINUTES.sleep(10);
     }
 }
